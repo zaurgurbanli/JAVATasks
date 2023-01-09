ff77eb3cbe8483fe8c730d06c71d87c88b7955db
zaurgurbanli <zaurqurbanli@gmail.com>
2023-01-09 02:33:18 +0100

    adding comments to hw5

--- hw5/truthTable/TruthTable.java
+++ hw5/truthTable/TruthTable.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
 
 public class TruthTable {
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = reader.nextInt();
 
-        for (int i =0; i<Math.pow(2,n); i++){
+    public void generate(int n){
+        //iterate from 0 to 2^n
+        for (int i = 0; i < Math.pow(2, n); i++) {
+
+            //convert each number to binary
             String binary = Integer.toBinaryString(i);
+
             int length = binary.length();
 
-            if(length<n)
-                binary = "0".repeat(n-length)+binary;
+            //add zeros to the left of binary if it takes less than n bit
+            if (length < n)
+                binary = "0".repeat(n - length) + binary;
 
             System.out.println(binary);
         }
+    }
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n= reader.nextInt();
+
+        TruthTable table = new TruthTable();
+        table.generate(n);
+
+
+
     }
 }
